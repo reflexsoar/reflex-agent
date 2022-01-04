e7baed1a2d9da1b205a7f9fb6bd5833c2333503f
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-01-03 23:34:11 -0500

    get with the program...

--- reflex-agent.py
+++ reflex-agent.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     parser.add_option('--proxy', dest='proxy', type=str, action="store", help='If the agent is running behind a proxy you may need to set this')
     parser.add_option('--groups', dest='groups', type=str, action="store", help="The groups this agent should be a part of")
     parser.add_option('--cacert', dest='cacert', type=str, action="store", default=False, help="Path to the certificate authority certificate used for the Reflex API")
-    parser.add_option('--ignore-tls', dest='ignore_tls', action='store_true')
+    parser.add_option('--ignore-tls', dest='ignore_tls', action='store_false')
     (options,args) = parser.parse_args()
 
     # Override commandline arguments with environmental variables
