1eb6c3671aaceb5dab5888137cd9d08584f57225
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-10-18 13:58:52 -0400

    default agent roles if config is not handing them out properly

--- utils/base.py
+++ utils/base.py
@@ -341,6 +341,9 @@ class Agent(object):
         if response and response.status_code == 200:
             self.config = response.json()
 
+            if 'roles' not in self.config:
+                self.config['roles'] = []
+
             # If the policy has roles configured to override, override the direct assigned roles
             # on the agent
             if len(self.config['policy']['roles']) > 0:
