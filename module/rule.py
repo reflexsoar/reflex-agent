707a62c7f3b7341927acf0a0d9cdc9911ef8da9d
Brian Carroll <brian@hasecuritysolutions.com>
2023-08-12 23:54:01 -0400

    class based rule functionality

--- module/rule.py
+++ module/rule.py
@@ -1,10 +1,12 @@
+import json
 import time
 from loguru import logger
+
 
 class BaseRule:
     ''' Defines a base class used by all rule types '''
 
-    def __init__(self, detection: dict, detection_input, elastic = None, agent = None):
+    def __init__(self, detection: dict, detection_input, elastic=None, agent=None):
         ''' Initializes the rule type '''
 
         self.type_num = -1
@@ -12,105 +14,92 @@ class BaseRule:
 
         self.detection = detection
 
+        if not detection_input:
+            raise ValueError('Detection input is required')
+        self.detection_input = detection_input
+
         if not agent:
-            raise ValueError('Agent is required')
+            raise ValueError('Reflex Agent object is required')
         self.agent = agent
 
         if not elastic:
-            raise ValueError('elastic is required')
-        self.conn = elastic # Connection to the backend to query for data
+            raise ValueError(
+                'Elasticsearch/Opensearch connection object is required')
+        self.conn = elastic  # Connection to the backend to query for data
 
-        if not detection_input:
-            raise ValueError('Detection input is required')
-        self.detection_input = detection_input # The input for the detection
-
-        self.execution_time = 0.0 # Time taken to execute the detection
-        self.errors = [] # List of errors that occurred during execution
-        self.error = False # Whether or not an error occurred during execution
+        self.execution_time = 0.0  # Time taken to execute the detection
+        self.errors = []  # List of errors that occurred during execution
+        self.error = False  # Whether or not an error occurred during execution
+        self.timefield = '@timestamp'  # The timefield to use for the detection
         self.query = {
             "query": {
                 "bool": {
                     "must": []
                 }
             }
-        } # The query used to execute the detection
+        }  # The query used to execute the detection
+
+        self.set_base_filter()
+        self.set_time_range()
         self.set_exclusions()
 
-    def add_exclusion(self, exclusion: str):
-        ''' Adds an exclusion to the detection '''
+    def set_time_range(self):
+        """
+        Sets the initial time range for the detection
+        """
+        # Override the default timestamp field if one is specified in the detection
+        if hasattr(self.detection, 'alert_time_field'):
+            self.timefield = self.detection.alert_time_field
 
-        if 'list' in exclusion and exclusion['list']['uuid'] is not None:
-            list_values = self.agent.get_list_values(exclusion['list']['uuid'])
-            if exclusion['values'] is not None:
-                exclusion['values'].extend(list_values)
-            else:
-                exclusion['values'] = list_values
-
-        self.query['query']['bool']['must_not'].append({
-            "terms": {
-                f"{exclusion['field']}": exclusion['values']
+        self.query["query"]["bool"]["must"].append({
+            "range": {
+                f"{self.timefield}": {
+                    "gte": f"now-{self.detection.lookbehind}m"
+                }
             }
         })
 
-    def set_base_filter(self, query_filter: str):
+    def query_as_json(self):
+        """
+        Prints the query as JSON
+        """
+        print(json.dumps(self.query, indent=2))
+
+    def set_base_filter(self):
         ''' Sets the base query for the detection '''
-        self.query['query']['bool']['must'].append(query_filter)
+        self.query['query']['bool']['must'].append({
+            "query_string": {
+                "query": self.detection.query['query']}
+        })
 
     def set_exclusions(self):
         '''Sets the exclusions based on the detection'''
         if hasattr(self.detection, 'exceptions') and self.detection.exceptions != None:
             self.query["query"]["bool"]["must_not"] = []
-            for exclusion in self.detection.exceptions:
-                self.add_exclusion(exclusion)
+            for exception in self.detection.exceptions:
 
+                if 'condition' in exception and exception['condition'] == 'wildcard':
+                    for value in exception['values']:
+                        self.query["query"]["bool"]["must_not"].append(
+                            {
+                                "wildcard": {
+                                    f"{exception['field']}": value
+                                }
+                            }
+                        )
+                else:
+                    if 'list' in exception and exception['list']['uuid'] != None:
+                        list_values = self.agent.get_list_values(
+                            uuid=exception['list']['uuid'])
+                        exception['values'] = list_values
 
-    def run_query(self):
-        
-        docs = []
-        query_time = 0
-        scroll_size = 0
-        res = self.elastic.conn.search(
-            index=self.detection_input['config']['index'],
-            body=self.query,
-            scroll='2m'
-        )
-
-        scroll_id = res['_scroll_id']
-        if 'total' in res['hits']:
-            if len(res['hits']['hits']) > 0:
-                logger.info(
-                    f"{self.detection.name} ({self.detection.uuid}) - Found {len(res['hits']['hits'])} detection hits.")
-            query_time += res['took']
-            scroll_size = res['hits']['total']['value']
-
-            # Parse the events and extract observables, tags, signature the event
-            docs += self.elastic.conn.parse_events(
-                res['hits']['hits'], title=self.detection.name, signature_values=[self.detection.detection_id], risk_score=self.detection.risk_score)
-        else:
-            scroll_size = 0
-
-        # Scroll
-        while (scroll_size > 0):
-            logger.info(
-                f"{self.detection.name} ({self.detection.uuid}) - Scrolling Elasticsearch results...")
-            # TODO: Move scroll time to config
-            res = self.elastic.conn.scroll(
-                scroll_id=scroll_id, scroll='2m')
-            if len(res['hits']['hits']) > 0:
-                query_time += res['took']
-                logger.info(
-                    f"{self.detection.name} ({self.detection.uuid}) - Found {len(res['hits']['hits'])} detection hits.")
-                # Parse the events and extract observables, tags, signature the event
-                docs += self.elastic.parse_events(
-                    res['hits']['hits'], title=self.detection.name, signature_values=[self.detection.detection_id], risk_score=self.detection.risk_score)
-
-            scroll_size = len(res['hits']['hits'])
-
-        if len(docs) > 0 :
-            self.logger.info(
-                f"{self.detection.name} ({self.detection.uuid}) - Total Hits {len(docs)}")
-        return docs, query_time
-
+                    self.query["query"]["bool"]["must_not"].append(
+                        {
+                            "terms": {
+                                f"{exception['field']}": exception['values']
+                            }
+                        }
+                    )
 
     def run(self):
         ''' Runs the detection '''
---
+++ module/threshold.py
@@ -0,0 +1,129 @@
+from .rule import BaseRule
+
+class ThresholdRule(BaseRule):
+
+    def __init__(self, detection: dict, detection_input, elastic=None, agent=None):
+        super().__init__(detection, detection_input, elastic, agent)
+        self._config = self.detection.threshold_config
+        self.create_piped_aggregation()
+        self.query["size"] = 0
+
+    def execute(self):
+        data =  self.conn.search(index=self.detection_input['index'], body=self.query)
+
+        operator = self._config['operator']
+        threshold = self._config['threshold']
+        key_fields = self._config['key_field']
+        threshold_field = self._config['threshold_field']
+        rule_name = self.detection.name
+        mode = 'cardinality' if self._config['cardinality'] else 'count'
+
+        if len(key_fields) == 0:
+            hits = data['hits']['total']['value']
+            if self.test_threshold(hits, operator, threshold):
+                print(f"[!] Rule \"{rule_name}\" has matched - {hits} {operator} {threshold}")
+
+        if len(key_fields) == 1:
+            threshold_values = self.get_bucket_count(key_fields, key_fields[0], data['aggregations'], mode)
+            if mode == 'cardinality':
+                if self.test_threshold(len(threshold_values), operator, threshold):
+                    print(f"[!] Rule \"{rule_name}\" has matched - {len(threshold_values)} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
+            else:
+                for threshold_value in threshold_values:
+                    if self.test_threshold(threshold_value['doc_count'], operator, threshold):
+                        print(f"[!] {threshold_value['key']} has matched rule \"{rule_name}\" - {threshold_value['doc_count']} {operator} {threshold}")
+
+        elif len(key_fields) >= 2:
+            for bucket in data['aggregations'][key_fields[0]]['buckets']:
+                threshold_values = self.get_bucket_count(key_fields[1:], threshold_field, bucket, mode)
+                if mode == 'cardinality':
+                    if self.test_threshold(len(threshold_values), operator, threshold):
+                        print(f"[!] {bucket['key']} has matched rule \"{rule_name}\" - {len(threshold_values)} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
+                else:
+                    for threshold_value in threshold_values:
+                        if self.test_threshold(threshold_value['doc_count'], operator, threshold):
+                            print(f"[!] {bucket['key']} has matched rule \"{rule_name}\" - {threshold_value['doc_count']} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
+
+    def test_threshold(self, count, operator, threshold):
+        """
+        Test the count against the threshold and return true if the condition is met
+        """
+
+        if operator == '>':
+            return count > threshold
+        elif operator == '>=':
+            return count >= threshold
+        elif operator == '<':
+            return count < threshold
+        elif operator == '<=':
+            return count <= threshold
+        elif operator == '==':
+            return count == threshold
+        else:
+            return False
+        
+    def get_bucket_count(self, key_fields, threshold_field, data, mode):
+        """ Collapse the buckets and keys in to a new dictionary
+        by diving in to nested buckets until we reach the threshold field
+        """
+
+        threshold_values = []
+        
+        if threshold_field in data:
+            threshold_values = data[threshold_field]['buckets']
+            return threshold_values
+        else:
+            for key in key_fields:
+                if key in data and 'buckets' in data[key]:
+                    for bucket in data[key]['buckets']:
+                        if threshold_field in bucket:
+                            return bucket[threshold_field]['buckets']
+                        else:
+                            threshold_values = self.get_bucket_count(key_fields[1:], threshold_field, bucket, mode)
+
+        return threshold_values
+        
+    def create_piped_aggregation(self):
+        '''
+        Automatically creates a piped aggregation for the given fields.
+        '''
+        def create_terms_aggs(level, fields, threshold, max_size):
+            
+
+            if level >= len(fields):
+                return None
+            
+            agg = {
+                f"{fields[level]}": {
+                    "terms": {
+                        "field": fields[level],
+                        "size": max_size
+                    }
+                }
+            }
+
+            #if threshold > 0:
+            #    agg[f"{fields[level]}"]["terms"]["min_doc_count"] = threshold
+
+            # If this is the second to last field, add a top_hits aggregation
+            if level == len(fields)-2:
+                agg[f"{fields[level]}"]["aggs"] = {
+                    "top_hits": {
+                        "top_hits": {
+                            "size": 1
+                        }
+                    }
+                }
+
+            if level < len(fields)+1:
+                aggs = create_terms_aggs(level + 1, fields, threshold, max_size)
+                if aggs:
+                    agg[f"{fields[level]}"]["aggs"] = aggs
+                
+            return agg
+
+        aggs = create_terms_aggs(0, self._config['key_field'],
+                                 self._config['threshold'],
+                                 100)
+        if aggs:
+            self.query['aggs'] = aggs
