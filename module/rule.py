17fb6fc5b76f9dabf164cb4f2d733213cd92ae42
Brian Carroll <brian@hasecuritysolutions.com>
2023-08-15 15:36:28 -0400

    threshold rule enhancements

--- module/rule.py
+++ module/rule.py
@@ -1,18 +1,28 @@
+import os
 import json
 import time
-from loguru import logger
+from datetime import datetime
+from utils.elasticsearch import Elastic
+from opensearchpy.helpers import bulk
 
 
 class BaseRule:
     ''' Defines a base class used by all rule types '''
 
-    def __init__(self, detection: dict, detection_input, elastic=None, agent=None):
+    def __init__(self, detection: dict,
+                 detection_input,
+                 credential, agent=None,
+                 signature_fields=[],
+                 field_mapping={}):
         ''' Initializes the rule type '''
 
         self.type_num = -1
         self.type_name = 'base'
 
         self.detection = detection
+        self.credential = credential
+        self.signature_fields = signature_fields
+        self.field_mapping = field_mapping
 
         if not detection_input:
             raise ValueError('Detection input is required')
@@ -22,12 +32,10 @@ class BaseRule:
             raise ValueError('Reflex Agent object is required')
         self.agent = agent
 
-        if not elastic:
-            raise ValueError(
-                'Elasticsearch/Opensearch connection object is required')
-        self.conn = elastic  # Connection to the backend to query for data
+        self.conn = None
 
         self.execution_time = 0.0  # Time taken to execute the detection
+        self.query_time = 0.0
         self.errors = []  # List of errors that occurred during execution
         self.error = False  # Whether or not an error occurred during execution
         self.timefield = '@timestamp'  # The timefield to use for the detection
@@ -39,9 +47,94 @@ class BaseRule:
             }
         }  # The query used to execute the detection
 
+        self.build_connection()  # Connection to the backend to query for data
         self.set_base_filter()
         self.set_time_range()
-        self.set_exclusions()
+        self.build_exceptions()
+
+    def suppress_events(self, events):
+        '''
+        Reduces the number of events by grouping the events by the signature and
+        only returning max_events per signature
+        '''
+        # Group the events by signature
+        grouped_events = {}
+
+        max_events = 0
+        if hasattr(self.detection, 'suppression_max_events'):
+            max_events = self.detection.suppression_max_events
+
+        if max_events <= 0:
+            return events
+
+        for event in events:
+            if event.signature not in grouped_events:
+                grouped_events[event.signature] = []
+            if len(grouped_events[event.signature]) < max_events:
+                grouped_events[event.signature].append(event)
+
+        # Coallesce the events for each signature back in to a single list
+        _events = []
+        for signature in grouped_events:
+            _events.extend(grouped_events[signature])
+
+        return _events
+    
+    def ship_docs(self, docs):
+        """
+        Sends the documents as Events to the API via the Agent
+        """
+
+        for doc in docs:
+            doc.description = getattr(
+                self.detection, 'description', 'No description provided')
+            doc.tags += getattr(self.detection, 'tags', [])
+            doc.severity = getattr(self.detection, 'severity', 1)
+            doc.detection_id = getattr(self.detection, 'uuid', None)
+            doc.input_uuid = self.detection_input['uuid']
+
+        if len(docs) > 0:
+
+            # Write the docs to Elasticsearch if there are any
+            # and the writeback is enabled
+            self.writeback(docs)
+
+            # If the detection has suppression_max_events set to something other than 0
+            # suppress the events
+            docs = self.suppress_events(docs)
+            
+            # If not dropping the event, process the hits
+            if not self.drop:
+                self.agent.process_events(docs, True)
+
+    def writeback(self, events):
+        # If the environment variable for writeback_index is set, write the results to the index
+        # using the bulk helper and reusing the elastic.conn connection object
+        if os.getenv('REFLEX_DETECTIONS_WRITEBACK_INDEX') != None:
+            self.logger.info(
+                f"Writing {len(events)} events to {os.getenv('REFLEX_DETECTIONS_WRITEBACK_INDEX')}")
+            bulk(self.conn, events, index=os.getenv(
+                'REFLEX_DETECTIONS_WRITEBACK_INDEX'))
+
+    @property
+    def drop(self):
+        # If the environment variable for drop_events is set, drop the events
+        if os.getenv('REFLEX_DETECTIONS_DROP_EVENTS') != None:
+            self.logger.info(
+                f"The REFLEX_DETECTIONS_DROP_EVENTS environment variable is set.  Dropping events.")
+            return True
+        return False
+
+    def build_connection(self):
+        """
+        Returns a connection object to ElasticSearch
+        """
+        self.elastic = Elastic(
+            self.detection_input['config'],
+            self.field_mapping,
+            self.credential,
+            self.signature_fields
+        )
 
     def set_time_range(self):
         """
@@ -72,7 +165,7 @@ class BaseRule:
                 "query": self.detection.query['query']}
         })
 
-    def set_exclusions(self):
+    def build_exceptions(self):
         '''Sets the exclusions based on the detection'''
         if hasattr(self.detection, 'exceptions') and self.detection.exceptions != None:
             self.query["query"]["bool"]["must_not"] = []
@@ -107,5 +200,25 @@ class BaseRule:
         # Start the timer
         start_timer = time.time()
 
-        # Run the detection
-        print(self.query)
+        docs = self.execute()
+        #print(docs)
+        if docs:
+            self.ship_docs(docs)
+
+        end_timer = time.time()
+
+        total_hits = self.detection.total_hits if self.detection.total_hits else 0
+
+        update_payload = {
+            'last_run': self.detection.last_run,
+            'hits': len(docs),
+            'time_taken': (end_timer - start_timer)*1000,
+            'query_time_taken': self.query_time,
+            'total_hits': total_hits + len(docs),
+            'last_hit': datetime.utcnow().isoformat()
+        }
+
+        self.agent.update_detection(self.detection.uuid, payload=update_payload)
+
+        # Close the connection to Elasticsearch
+        self.elastic.conn.transport.close()
--- module/threshold.py
+++ module/threshold.py
@@ -1,57 +1,99 @@
+from concurrent.futures import ThreadPoolExecutor
+import copy
 from .rule import BaseRule
-
+from loguru import logger
 
 class ThresholdRule(BaseRule):
 
-    def __init__(self, detection: dict, detection_input, elastic=None, agent=None):
-        super().__init__(detection, detection_input, elastic, agent)
+    def __init__(self, detection: dict,
+                 detection_input,
+                 credential, agent=None,
+                 signature_fields=[],
+                 field_mapping={}):
+        super().__init__(detection, detection_input, credential, agent, signature_fields, field_mapping)
         self._config = self.detection.threshold_config
-        self.create_piped_aggregation()
+        self.create_multi_term_aggregation()
         self.query["size"] = 0
 
     def execute(self):
-        data = self.conn.search(
-            index=self.detection_input['index'], body=self.query)
+
+        docs = []
+
+        data = self.elastic.conn.search(
+            index=self.detection_input['config']['index'], body=self.query)
+        
+        if 'took' in data:
+            self.query_time += data['took']
 
         operator = self._config['operator']
         threshold = self._config['threshold']
         key_fields = self._config['key_field']
         threshold_field = self._config['threshold_field']
         rule_name = self.detection.name
-        mode = 'cardinality' if self._config['cardinality'] else 'count'
+        mode = self._config['mode']
 
-        if len(key_fields) == 0:
-            hits = data['hits']['total']['value']
-            if self.test_threshold(hits, operator, threshold):
-                print(
-                    f"[!] Rule \"{rule_name}\" has matched - {hits} {operator} {threshold}")
+        doc_searches = []
 
-        if len(key_fields) == 1:
-            threshold_values = self.get_bucket_count(
-                key_fields, key_fields[0], data['aggregations'], mode)
+        try:
+            hits = False
             if mode == 'cardinality':
-                if self.test_threshold(len(threshold_values), operator, threshold):
-                    print(
-                        f"[!] Rule \"{rule_name}\" has matched - {len(threshold_values)} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
-            else:
-                for threshold_value in threshold_values:
-                    if self.test_threshold(threshold_value['doc_count'], operator, threshold):
-                        print(
-                            f"[!] {threshold_value['key']} has matched rule \"{rule_name}\" - {threshold_value['doc_count']} {operator} {threshold}")
-
-        elif len(key_fields) >= 2:
-            for bucket in data['aggregations'][key_fields[0]]['buckets']:
-                threshold_values = self.get_bucket_count(
-                    key_fields[1:], threshold_field, bucket, mode)
-                if mode == 'cardinality':
-                    if self.test_threshold(len(threshold_values), operator, threshold):
-                        print(
-                            f"[!] {bucket['key']} has matched rule \"{rule_name}\" - {len(threshold_values)} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
+                if 'buckets' in data['aggregations']["1"]:
+                    for bucket in data['aggregations']["1"]["buckets"]:
+                        value = bucket[threshold_field]['value']
+                        if self.test_threshold(value, operator, threshold):
+                            hits = True
+                            # Zip the bucket key_fields and bucket keys together
+                            # and create a dictionary of the key_fields and bucket keys
+                            key_values = dict(zip(key_fields, bucket['key']))
+                            doc_searches.append(key_values)
+                            logger.warning(f"[!] Rule \"{rule_name}\" has matched - {value} {operator} {threshold} - {bucket['key']}")
                 else:
-                    for threshold_value in threshold_values:
-                        if self.test_threshold(threshold_value['doc_count'], operator, threshold):
-                            print(
-                                f"[!] {bucket['key']} has matched rule \"{rule_name}\" - {threshold_value['doc_count']} {operator} {threshold} - {','.join([str(x['key']) for x in threshold_values])}")
+                    value = data['aggregations']["1"]["value"]
+                    if self.test_threshold(value, operator, threshold):
+                        hits = True
+                        doc_searches.append({})
+                        logger.warning(f"[!] Rule \"{rule_name}\" has matched - {value} {operator} {threshold}")
+            elif mode == 'terms':
+                if 'aggregations' in data:
+                    for bucket in data['aggregations']["1"]["buckets"]:
+                        if len(key_fields) == 1:
+                            value = bucket['doc_count']
+                            if self.test_threshold(value, operator, threshold):
+                                hits = True
+                                doc_searches.append(dict(zip([threshold_field], [bucket['key']])))
+                                logger.warning(f"[!] Rule \"{rule_name}\" has matched - {value} {operator} {threshold} - {bucket['key']}")
+                        else:
+                            for _bucket in bucket[threshold_field]['buckets']:
+                                value = _bucket['doc_count']
+                                if self.test_threshold(value, operator, threshold):
+                                    hits = True
+                                    key_values = dict(zip([threshold_field], [_bucket['key']]))
+                                    doc_searches.append(key_values)
+                                    logger.warning(f"[!] Rule \"{rule_name}\" has matched - {value} {operator} {threshold} - {bucket['key']} to {_bucket['key']}")
+            elif mode == 'count':
+                value = data['hits']['total']['value']
+                if self.test_threshold(value, operator, threshold):
+                    hits = True
+                    logger.warning(f"[!] Rule \"{rule_name}\" has matched - {value} {operator} {threshold}")
+                #print(json.dumps(data['aggregations'], indent=2))
+
+            if not hits:
+                logger.success(f"[i] Rule \"{rule_name}\" did not match")
+        except KeyError as e:
+            logger.error(f"[!] Rule \"{rule_name}\" failed to match")
+            logger.error(e)
+            logger.error(data)
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            futures = [executor.submit(self.fetch_documents, fields_values=doc_search, threshold_field=threshold_field) for doc_search in doc_searches]
+            for future in futures:
+                docs.extend(future.result())
+
+        docs = self.elastic.parse_events(docs, title=self.detection.name, signature_values=[
+                                    self.detection.detection_id], risk_score=self.detection.risk_score)
+        
+        return docs 
+            
 
     def test_threshold(self, count, operator, threshold):
         """
@@ -92,48 +134,141 @@ class ThresholdRule(BaseRule):
                                 key_fields[1:], threshold_field, bucket, mode)
 
         return threshold_values
+    
+    def fetch_documents(self, fields_values, threshold_field):
+        """ Fetch the documents that match the query and return the results
+        """
 
-    def create_piped_aggregation(self):
+        documents = []
+
+        keys = list(fields_values.keys())
+        top_hits_size = 1
+
+        if len(keys) == 1 and threshold_field == keys[0]:
+            top_hits_size = self._config['max_events']
+
+        query = copy.deepcopy(self.query)
+
+        # Set the size to max_size 
+        #query['size'] = self._config['max_events']
+
+        # Remove the aggregations
+        if 'aggs' in query:
+            del query['aggs']
+
+        # Update the base_query to include the fields and values
+        for field, value in fields_values.items():
+            query['query']['bool']['must'].append({
+                "match": {
+                    field: value
+                }
+            })
+        
+        # Replace the aggs with a terms aggregation on the threshold_field and a top_hits aggregation
+        query['aggs'] = {
+            "1": {
+                "terms": {
+                    "field": threshold_field,
+                    "size": self._config['max_events']
+                },
+                "aggs": {
+                    "hits": {
+                        "top_hits": {
+                            "size": top_hits_size
+                        }
+                    }
+                }
+            }
+        }
+
+        import json
+        #print(json.dumps(query, indent=2))
+
+        # Run the query
+        data = self.elastic.conn.search(
+            index=self.detection_input['config']['index'], body=query)
+        
+        #if 'took' in data:
+        #    self.query_time += data['took']
+        
+        # Loop through the buckets and append the hits to the documents list
+        for bucket in data['aggregations']['1']['buckets']:
+            if bucket['hits']['hits']['total']['value'] > 0:
+                documents.extend(bucket['hits']['hits']['hits'])
+        return documents
+    
+    def create_multi_term_aggregation(self):
         '''
-        Automatically creates a piped aggregation for the given fields.
+        Automatically creates a multi_term aggregation with the threshold_field being a piped aggregation
+        of the multi_term aggregation.
         '''
-        def create_terms_aggs(level, fields, threshold, max_size):
 
-            if level >= len(fields):
-                return None
+        fields = self._config['key_field']
+        threshold = self._config['threshold']
+        max_size = 100
+        mode = self._config['mode']
+        threshold_field = self._config['threshold_field']
 
+        if threshold_field is None:
+            threshold_field = fields[-1]
+
+        if len(fields) == 1:
             agg = {
-                f"{fields[level]}": {
-                    "terms": {
-                        "field": fields[level],
-                        "size": max_size
+                "1": {
+                    mode: {
+                        "field": fields[0]
+                        
                     }
                 }
             }
 
-            # if threshold > 0:
-            #    agg[f"{fields[level]}"]["terms"]["min_doc_count"] = threshold
-
-            # If this is the second to last field, add a top_hits aggregation
-            if level == len(fields)-2:
-                agg[f"{fields[level]}"]["aggs"] = {
-                    "top_hits": {
-                        "top_hits": {
-                            "size": 1
+            if mode == "terms":
+                agg["1"][mode]["size"] = max_size
+                agg["1"][mode]["min_doc_count"] = threshold
+        
+        elif len(fields) == 2 and threshold_field == fields[1]:
+            agg = {
+                "1": {
+                    "terms": {
+                        "field": fields[0],
+                        "size": max_size
+                    },
+                    "aggs": {
+                        threshold_field: {
+                            mode: {
+                                "field": fields[1]
+                            }
                         }
                     }
                 }
+            }
 
-            if level < len(fields)+1:
-                aggs = create_terms_aggs(
-                    level + 1, fields, threshold, max_size)
-                if aggs:
-                    agg[f"{fields[level]}"]["aggs"] = aggs
+            if mode == "terms":
+                agg["1"]["aggs"][threshold_field]["terms"]["size"] = max_size
+                agg["1"]["aggs"][threshold_field]["terms"]["min_doc_count"] = threshold   
 
-            return agg
+        else:
+            if threshold_field == fields[-1]:
+                fields = fields[0:-1]
+                
+            agg = {
+                "1": {
+                    "multi_terms": {
+                        "terms": [{"field": f} for f in fields[0:len(fields)]]
+                    },
+                    "aggs": {
+                        threshold_field: {
+                            mode: {
+                                "field": threshold_field
+                            }
+                        }
+                    }
+                }
+            }
 
-        aggs = create_terms_aggs(0, self._config['key_field'],
-                                 self._config['threshold'],
-                                 100)
-        if aggs:
-            self.query['aggs'] = aggs
+            if mode == "terms":
+                agg["1"]["aggs"][threshold_field]["terms"]["size"] = max_size
+                agg["1"]["aggs"][threshold_field]["terms"]["min_doc_count"] = threshold
+
+        if agg:
+            self.query['aggs'] = agg
